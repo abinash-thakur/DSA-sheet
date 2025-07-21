ed1576e861458e3939fbf94aed65474b3bd2c206
abinash <thakurabinash299@gmail.com>
2025-07-21 09:09:57 +0530

    adding the new list of data

---
+++ String/Longest.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Longest{
+  public static void main(String[] args){
+    HashMap<Character, Integer> data = new HashMap<>();
+    int max = Integer.MIN_VALUE;
+    String str = "abcabcdabcde";
+    int len = str.length;
+    int l = 0;
+    int r = 0;
+    
+  }
+}
